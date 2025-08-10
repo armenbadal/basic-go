97840579e980fd1200c7820b94cd4e2a07f3744c
Արմեն Բադալյան <armenbadal@gmail.com>
2025-08-10 21:24:24 +0400

    Փոքր շտկում թեսթում

--- basic/parser/parser_test.go
+++ basic/parser/parser_test.go
@@ -17,7 +17,7 @@ func TestOne(t *testing.T) {
 
 	tree, _ := pars.Parse()
 	if nil == tree {
-		t.Error("failed to parse the file")
+		t.Fatal("failed to parse the file")
 	}
 
 	if len(tree.Subroutines) != 3 {
